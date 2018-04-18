f6fc4509be525f8ba4285c5e79969cf3cc921c36
Maxwell A Palmer <mpalmer16@mail.csuchico.edu>
2018-04-17 22:03:54 -0700

    Sorts the numbers from largest to smallest

--- main.cpp
+++ main.cpp
@@ -1,5 +1,43 @@
 #include <iostream>
+#include <vector>
+#include <string>
 using namespace std;
+
+void sortNums(int &,int &, int&);
+
+void sortNums(int &r,int &g,int &b)
+{
+  const int redNum = r;
+  const int greenNum = g;
+  const int blueNum = b;
+  if(redNum < greenNum && redNum > blueNum)
+  {
+    r = greenNum;
+  }
+  else if(redNum < greenNum && redNum < blueNum)
+  {
+    r = blueNum;
+  }
+
+  if(greenNum > redNum && greenNum > blueNum)
+  {
+    g = redNum;
+  }
+  else if(greenNum < redNum && greenNum < blueNum)
+  {
+    g = blueNum;
+  }
+
+  if (blueNum > redNum && blueNum > greenNum)
+  {
+    b = redNum;
+  }
+  
+  else if(blueNum < redNum && blueNum > greenNum)
+  {
+    b = greenNum;
+  }
+}
 
 int main()
 {
@@ -9,9 +47,7 @@ int main()
   cin>>red>>green>>blue;
   //...END OF "DO NOT CHANGE" AREA
 
-
-
-
+  sortNums(red, green, blue);
 
   //DO NOT CHANGE WITHIN THIS AREA...
   cout<<"Rearranged....\n";
